1194638cdd0ebed012a3861555e95e2d7a785309
bilalm354 <bilalm354@gmail.com>
2022-02-19 23:50:09 +0000

    removed logo find and added some links

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div className='Nav-bar'>
+          <a href='https://docs.google.com/document/d/1FHzaHQE2R8jK0bv6rl2TZqD8Hq2swN-KYv-3TuFMG74/edit?usp=sharing'>
+            CV
+          </a>
+          <a href='https://docs.google.com/document/d/1FHzaHQE2R8jK0bv6rl2TZqD8Hq2swN-KYv-3TuFMG74/export?format=pdf'>
+            CV (PDF)
+          </a>
+          <a href='www.google.com'>
+            Portfolio
+          </a>
+          <a href='www.google.com'>
+            Contact
+          </a>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
