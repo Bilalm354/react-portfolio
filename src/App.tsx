2252bd2d74a6735d02add9c6ef692ec564fcc685
bilalm354 <bilalm354@gmail.com>
2022-02-19 23:54:38 +0000

    added airbnb style eslint

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className='Nav-bar'>
-          <a href='https://docs.google.com/document/d/1FHzaHQE2R8jK0bv6rl2TZqD8Hq2swN-KYv-3TuFMG74/edit?usp=sharing'>
+        <div className="Nav-bar">
+          <a href="https://docs.google.com/document/d/1FHzaHQE2R8jK0bv6rl2TZqD8Hq2swN-KYv-3TuFMG74/edit?usp=sharing">
             CV
           </a>
-          <a href='https://docs.google.com/document/d/1FHzaHQE2R8jK0bv6rl2TZqD8Hq2swN-KYv-3TuFMG74/export?format=pdf'>
+          <a href="https://docs.google.com/document/d/1FHzaHQE2R8jK0bv6rl2TZqD8Hq2swN-KYv-3TuFMG74/export?format=pdf">
             CV (PDF)
           </a>
-          <a href='www.google.com'>
+          <a href="http://www.google.com">
             Portfolio
           </a>
-          <a href='www.google.com'>
+          <a href="http://www.google.com">
             Contact
           </a>
         </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
